6bacdcd55a41313ad07b7a89ba6ea5fe8b0b2634
Paulo Varrone <paulorobertovarrone@gmail.com>
2023-05-05 21:01:57 -0300

    menu scoll slide up

--- script.js
+++ script.js
@@ -1,28 +1,22 @@
-function mostrarEsconder() {
-  let toggle = this.nextElementSibling;
-  let nav = document.getElementById("nav");
+let nav = document.getElementById("nav");
+let menuToggle = document.querySelector('.toggle');
 
-  if (nav.style.display == "flex" || $(nav).is(":visible")) {
+menuToggle.onclick = function() {
+  menuToggle.classList.toggle('active');
+  if ($(nav).is(":visible")) {
     $(nav).slideUp();
   } else {
     $(nav).slideDown();
   }
-}
+};
 
-let menuToggle = document.querySelector('.toggle');
-  menuToggle.onclick = function() {
-  menuToggle.classList.toggle('active')
-    
-}  
-
-// add an eventListener scroll
-window.addEventListener("scroll", function() {
-  // hide menu and red (x) when scroll
-  nav.style.display = "none";
-  menuToggle.classList.remove('active');
+window.addEventListener('scroll', function() {
+  if ($(nav).is(":visible")) {
+    $(nav).slideUp();
+    menuToggle.classList.remove('active');
+  }
 });
 
-let toggleElements = document.querySelectorAll('.toggle');
-    for (let i = 0; i < toggleElements.length; i++) {
-    toggleElements[i].addEventListener('click', mostrarEsconder);
-}
+
+
+
